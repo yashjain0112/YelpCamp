03166f792a93c57d112d360574fff3650ad264f2
Yash Jain <yash.199301055@muj.manipal.edu>
2021-08-06 20:07:06 +0530

    Made few changes

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render(`errors`, { err });
 });
 
-app.listen(3000, () => {
-  console.log(`Listening on port : 3000`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port : ${port}`);
 });
