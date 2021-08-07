7635e7a1eff851fe3baca2e5dc994b6478a17376
Yash Jain <yash.199301055@muj.manipal.edu>
2021-08-07 11:15:15 +0530

    Final changes to map and views

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "cluster-map",
   style: "mapbox://styles/mapbox/light-v10",
-  center: [-103.5917, 40.6699],
+  center: [77.1025, 28.7041],
   zoom: 3,
 });
 
